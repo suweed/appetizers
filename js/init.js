d679ca17de725ebb03924cea4214d44de70b3489
usuario <usuario@Mac-mini-de-usuario.local>
2018-05-12 23:05:27 -0500

    pagina inicial

---
+++ js/init.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+    $(".item-menu").click(function() {
+        $(".hamburger").click();
+    });
+
+    $('#fullpage').fullpage({
+        anchors: ['home', 'menu', 'gallery', 'reservation', 'contact'],
+        menu: '#menu-fullpage',
+        scrollingSpeed: 1000,
+        onLeave: function(index, nextIndex, direction) {
+            var leavingSection = $(this);
+            var tl2 = new TimelineMax();
+            var titleContent = $(".title-content-home");
+
+            //after leaving section 1
+            if (index == 1 && direction == 'down') {
+                console.log("Going to section 2!");
+
+                tl2.to(titleContent, 2, { top: 100, bottom: 0, autoAlpha: 0 });
+
+            } else if (index == 2 && direction == 'up') {
+                console.log("Going to section 1!");
+
+            }
+
+        },
+        afterLoad: function(anchorLink, index) {
+            var loadedSection = $(this);
+            var titleContent = $(".title-content-home");
+            var tl = new TimelineMax();
+
+            if (index == 1) {
+                console.log("en section 1");
+
+                tl.fromTo(titleContent, 2, { bottom: -100 }, { top: 0, autoAlpha: 0.7 });
+            }
+        }
+    });
+});
