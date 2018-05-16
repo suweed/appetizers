51d9e6f581d3e71a921ab8b9114330217ec5d441
suweed <gsuskr2o@gmail.com>
2018-05-16 02:37:48 -0500

    gallery first fase

--- js/init.js
+++ js/init.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
         anchors: ['home', 'menu', 'gallery', 'buffet', 'reservation', 'contact'],
         menu: '#menu-fullpage',
         scrollingSpeed: 1000,
+        normalScrollElements: '.scroll-active',
         onLeave: function(index, nextIndex, direction) {
             var leavingSection = $(this);
             var tl2 = new TimelineMax();
@@ -20,6 +21,11 @@ $(document).ready(function() {
 
             }
 
+            if(index == 2 && direction == "down"){
+                console.log("going to section 3");
+
+            }
+
         },
         afterLoad: function(anchorLink, index) {
             var loadedSection = $(this);
@@ -31,8 +37,42 @@ $(document).ready(function() {
 
                 tl.fromTo(titleContent, 2, { bottom: -100 }, { top: 0, autoAlpha: 0.7 });
             }
+
+            if(index == 3){
+                // Get the modal
+                var modal = document.getElementById('myModal');
+                // Get the image and insert it inside the modal - use its "alt" text as a caption
+                var img = $(".pick-select");
+                var modalImg = document.getElementById("img01");
+                var captionText = document.getElementById("caption");
+                img.click(function(){
+                    modal.style.display = "block";
+                    modalImg.src = this.src;
+                    captionText.innerHTML = this.alt;
+                    $("label .menu").hide();
+                });
+
+                // Get the <span> element that closes the modal
+                var span = document.getElementsByClassName("close")[0];
+
+                // When the user clicks on <span> (x), close the modal
+                span.onclick = function() { 
+                    modal.style.display = "none";
+                    $("label .menu").show();
+                }
+            }
         }
     });
+
+    $(window).resize(function(){
+        updateScrollingClasses();
+    });
+
+    $(window).bind('mousewheel', function(){
+        updateScrollingClasses();
+    });
+
+    updateScrollingClasses();
 
     $(".item-menu").click(function() {
         $(".hamburger").click();
@@ -46,3 +86,20 @@ $(document).ready(function() {
         $.fn.fullpage.moveTo('menu', slide);
     });
 });
+
+(function($) {
+    $.fn.hasScrollBar = function() {
+        return this.get(0).scrollHeight > this.height();
+    }
+})(jQuery);
+
+function updateScrollingClasses(){
+    $('.section-main').each(function(index) {
+        var element = $(this);
+        if (element.hasScrollBar()) {
+            element.addClass('scroll-active');
+        } else {
+            element.removeClass('scroll-active');
+        }
+    });
+}
